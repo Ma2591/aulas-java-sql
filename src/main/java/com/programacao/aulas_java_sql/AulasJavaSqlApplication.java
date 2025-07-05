14eebddca6ae642b30e6ac04139c7d7dd7d6de99
Mariana <mari_oli25@hotmail.com>
2025-07-05 11:55:52 -0300

    Inicializando Projeto Maven

---
+++ src/main/java/com/programacao/aulas_java_sql/AulasJavaSqlApplication.java
@@ -0,0 +1,13 @@
+package com.programacao.aulas_java_sql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulasJavaSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulasJavaSqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/programacao/aulas_java_sql/AulasJavaSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programacao.aulas_java_sql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulasJavaSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
